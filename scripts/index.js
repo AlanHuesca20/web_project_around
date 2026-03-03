cc938cf12f349f9f47a9bd4c439eeddfbb76279d
Alan Huesca Salinas <alanhuesca2002@gmail.com>
2026-03-02 21:17:27 -0600

    script1

---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+console.log("java" + "script");
+let buttonEdit = document.querySelector(".profile__button-edit");
+let formEdit = document.querySelector(".popup__form");
+buttonEdit.addEventListener("click", openformEdit);
