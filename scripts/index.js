4f7ab538b947a5f378a7b3b82696cdc4f81dff9a
Alan Huesca Salinas <alanhuesca2002@gmail.com>
2026-03-02 22:12:37 -0600

    buttontypeopen

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,17 @@
-console.log("java" + "script");
 let buttonEdit = document.querySelector(".profile__button-edit");
 let formEdit = document.querySelector(".popup__form");
-buttonEdit.addEventListener("click", openformEdit);
+let popupEdit = document.querySelector("#popup__edit");
+let popupOpen = document.querySelector("popup_open");
+let closeButtonEdit = popupEdit.querySelector("popup__button-typeclose");
+
+function openModal() {
+  popupEdit.classList.add("popup_open");
+}
+
+buttonEdit.addEventListener("click", openModal);
+
+function closeModal() {
+  popupEdit.classList.remove("popup_open");
+}
+
+closeButtonEdit.addEventListener("click, closeModal");
